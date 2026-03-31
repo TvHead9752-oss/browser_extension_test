42a9068c258c62286b5280c7a78dc162ac935230
TvHead9752-oss <blaketheprofessor@gmail.com>
2026-03-30 21:31:30 -0400

    Removed two tests.

--- playwright.config.ts
+++ playwright.config.ts
@@ -50,10 +50,12 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
+    /*
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+    */ 
 
     /* Test against mobile viewports. */
     // {
